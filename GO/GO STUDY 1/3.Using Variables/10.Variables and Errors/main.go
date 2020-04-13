5ff957c11a83836de7735dac0f833d20c7cbd36e
birdie <54265111+birdie16@users.noreply.github.com>
2020-04-13 21:10:45 +0530

    Add files via upload

---
+++ GO/GO STUDY 1/3.Using Variables/10.Variables and Errors/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+    // Remove the println line below because s1 and s2 have not yet been defined and they can't be printed
+    //println(s1)
+    
+    s1 := "Hi, "
+    s2 := "world"
+    
+    // Change the value that the variable was given when it was defined
+    s1 = "Hello, "
+
+    // Since the s1 variable is a string type, remove the line below
+    //s1 = 100
+    
+    // Print the values of variables s1 and s2
+    println(s1,s2)
+    
+}
